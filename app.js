38886cb918378efece657e165acb6dc13fd816ad
IñakiOrozco <greenvana14@gmail.com>
2021-11-08 20:20:45 -0600

    Update app.js

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ app.use('/assets', express.static(path.join(__dirname, 'public')));
 //DB
 const { Db } = require('mongodb');
 const mongoose = require('mongoose');
+//const uri = "mongodb+srv://"+process.env.DB+"/Cluster0?retryWrites=true&w=majority"
 const uri = "mongodb+srv://admin:admin@cluster0.cfrb0.mongodb.net/Cluster0?retryWrites=true&w=majority";
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) => console.log("connected to db..."))
